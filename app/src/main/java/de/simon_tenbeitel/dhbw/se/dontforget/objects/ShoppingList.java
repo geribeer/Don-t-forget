1318cf33f83e5b891da04800f101af6e9831e361
Simon Tenbeitel <simon.tenbeitel@gmail.com>
2015-04-19 15:10:42 +0200

    Add UUID to ParseObjects for local db

--- app/src/main/java/de/simon_tenbeitel/dhbw/se/dontforget/objects/ShoppingList.java
+++ app/src/main/java/de/simon_tenbeitel/dhbw/se/dontforget/objects/ShoppingList.java
@@ -4,29 +4,41 @@ import com.parse.ParseClassName;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import java.util.UUID;
+
 /**
  * Created by Simon on 19.04.2015.
  */
 @ParseClassName("ShoppingList")
 public class ShoppingList extends ParseObject {
 
-    private static final String TITLE = "title";
-    private static final String COUNT = "count";
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_COUNT = "count";
+    private static final String KEY_UUID = "uuid";
 
     public String getTitle() {
-        return getString(TITLE);
+        return getString(KEY_TITLE);
     }
 
     public void setTitle(String title) {
-        put(TITLE, title);
+        put(KEY_TITLE, title);
     }
 
     public int getItemCount() {
-        return getInt(COUNT);
+        return getInt(KEY_COUNT);
     }
 
     public void setItemCount(int itemCount) {
-        put(COUNT, itemCount);
+        put(KEY_COUNT, itemCount);
+    }
+
+    public void setUuidString() {
+        UUID uuid = UUID.randomUUID();
+        put(KEY_UUID, uuid.toString());
+    }
+
+    public String getUuidString() {
+        return getString(KEY_UUID);
     }
 
     public static ParseQuery<ShoppingList> getQuery() {
--- app/src/main/java/de/simon_tenbeitel/dhbw/se/dontforget/objects/ShoppingListItem.java
+++ app/src/main/java/de/simon_tenbeitel/dhbw/se/dontforget/objects/ShoppingListItem.java
@@ -4,38 +4,50 @@ import com.parse.ParseClassName;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
+import java.util.UUID;
+
 /**
  * Created by Simon on 19.04.2015.
  */
 @ParseClassName("ShoppingListItem")
 public class ShoppingListItem extends ParseObject {
 
-    private static final String LIST_ID = "list";
-    private static final String TITLE = "title";
-    private static final String DONE = "done";
+    private static final String KEY_LIST_ID = "list";
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_DONE = "done";
+    private static final String KEY_UUID = "uuid";
 
     public String getListId() {
-        return getString(LIST_ID);
+        return getString(KEY_LIST_ID);
     }
 
     public void setListId(String listId) {
-        put(LIST_ID, listId);
+        put(KEY_LIST_ID, listId);
     }
 
     public String getTitle() {
-        return getString(TITLE);
+        return getString(KEY_TITLE);
     }
 
     public void setTitle(String title) {
-        put(TITLE, title);
+        put(KEY_TITLE, title);
     }
 
     public boolean isDone() {
-        return getBoolean(DONE);
+        return getBoolean(KEY_DONE);
     }
 
     public void setDone(boolean done) {
-        put(DONE, done);
+        put(KEY_DONE, done);
+    }
+
+    public void setUuidString() {
+        UUID uuid = UUID.randomUUID();
+        put(KEY_UUID, uuid.toString());
+    }
+
+    public String getUuidString() {
+        return getString(KEY_UUID);
     }
 
     public static ParseQuery<ShoppingListItem> getQuery() {
