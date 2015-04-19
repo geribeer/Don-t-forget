21a4539c1cce84ee6e09c7df03b9ebf9fb226d42
Simon Tenbeitel <simon.tenbeitel@gmail.com>
2015-04-19 14:24:48 +0200

    Attach FAB to ListView

--- app/src/main/java/de/simon_tenbeitel/dhbw/se/dontforget/ui/activity/MainActivity.java
+++ app/src/main/java/de/simon_tenbeitel/dhbw/se/dontforget/ui/activity/MainActivity.java
@@ -3,14 +3,18 @@ package de.simon_tenbeitel.dhbw.se.dontforget.ui.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.ListFragment;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ListView;
 
+import com.melnykov.fab.FloatingActionButton;
 import com.parse.ParseUser;
 
 import butterknife.ButterKnife;
+import butterknife.InjectView;
 import butterknife.OnClick;
 import de.simon_tenbeitel.dhbw.se.dontforget.R;
 import de.simon_tenbeitel.dhbw.se.dontforget.ui.fragment.ShoppingListMasterFragment;
@@ -22,18 +26,27 @@ public class MainActivity extends ActionBarActivity {
 
     private static final String TAG_SHOPPINGLIST_MASTER_FRAGMENT = "shoppinglist_master";
 
+    @InjectView(R.id.new_shoppingList) FloatingActionButton newShoppingListButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.inject(this);
 
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
         if (savedInstanceState == null) {
-            FragmentManager fragmentManager = getSupportFragmentManager();
             fragmentManager.beginTransaction()
                     .replace(R.id.fragment_shoppinglist_master_container, new ShoppingListMasterFragment(), TAG_SHOPPINGLIST_MASTER_FRAGMENT)
                     .commit();
         }
+
+        // Attach the floating action button to the ListView, so it can react to scrolling events.
+        // Becomes visible when an attached target is scrolled up and invisible when scrolled down.
+        ListFragment shoppingListFragment = (ListFragment) fragmentManager.findFragmentByTag(TAG_SHOPPINGLIST_MASTER_FRAGMENT);
+        ListView shoppingListListView = shoppingListFragment.getListView();
+        newShoppingListButton.attachToListView(shoppingListListView);
     }
 
     @Override
