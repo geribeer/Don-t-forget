80617a663967a11d8cb4ecb7567e8079715e1dba
Simon Tenbeitel <simon.tenbeitel@gmail.com>
2015-04-19 11:18:14 +0200

    Configured Parse for this app

--- app/src/main/java/de/simon_tenbeitel/dhbw/se/dontforget/DontforgetApplication.java
+++ app/src/main/java/de/simon_tenbeitel/dhbw/se/dontforget/DontforgetApplication.java
@@ -3,6 +3,8 @@ package de.simon_tenbeitel.dhbw.se.dontforget;
 import android.app.Application;
 import android.content.Context;
 
+import com.parse.Parse;
+
 /**
  * Created by Simon on 19.04.2015.
  */
@@ -14,6 +16,10 @@ public class DontforgetApplication extends Application {
     public void onCreate() {
         super.onCreate();
         sContext = getApplicationContext();
+
+        // Configure Parse
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, "c4Y0QwdstAcsrw8Ck4W2DUSwR0xh2sF7VXrb22kN", "DgDUeI0UL1gVfSgKGtn7KkBbMdAqE4tqrZaETh0o");
     }
 
     public static Context getContext(){
